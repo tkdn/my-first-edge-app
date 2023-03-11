807ae2d32199da37da9d364520b7050307241c00
tkdn <tkdnation@gmail.com>
2023-03-12 06:18:12 +0900

    impl counter

--- functions/api/[[route]].ts
+++ functions/api/[[route]].ts
@@ -1,11 +1,21 @@
 import { Hono } from "hono";
 import { handle } from "hono/cloudflare-pages";
+import { KVNamespace } from "@cloudflare/workers-types";
 
-const app = new Hono();
+type Env = {
+  Bindings: {
+    SAITAMAJS_KV: KVNamespace;
+  };
+};
 
-app.get("/api", (c) => {
+const app = new Hono<Env>();
+
+app.get("/api/counter", async (c) => {
+  const visited = await c.env.SAITAMAJS_KV.get("counter");
+  const count = Number(visited) + 1;
+  await c.env.SAITAMAJS_KV.put("counter", count.toString());
   return c.json({
-    message: "Hello",
+    counter: count,
   });
 });
 
