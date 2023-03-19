96c8b96c8d53d9a2c1b212da3b35173413ed116f
tkdn <tkdnation@gmail.com>
2023-03-20 00:21:41 +0900

    impl message api

--- functions/api/[[route]].ts
+++ functions/api/[[route]].ts
@@ -3,16 +3,20 @@ import { cors } from "hono/cors";
 import { handle } from "hono/cloudflare-pages";
 import { KVNamespace } from "@cloudflare/workers-types";
 import { counter } from "./handlers/counter";
+import { createMessage, getMessage } from "./handlers/message";
 
 export type Env = {
   Bindings: {
     SAITAMAJS_KV: KVNamespace;
     ORIGIN_HOST: string;
+    ["MICROCMS-API-KEY"]: string;
   };
 };
 
 const app = new Hono<Env>();
-app.use('*', cors({ origin: "*.saitamajs.pages.dev" }));
+app.use('*', cors({ origin: (origin) => origin }));
 app.get("/api/counter", counter);
+app.get("/api/message", getMessage);
+app.post("/api/message", createMessage);
 
 export const onRequest = handle(app);
---
+++ functions/api/handlers/message.ts
@@ -0,0 +1,29 @@
+import { Handler } from "hono/dist/types";
+import { Env } from "../[[route]]";
+
+const endpoint = "https://tkdn.microcms.io/api/v1/boards";
+
+export const getMessage: Handler<Env> = async (c) => {
+  const res = await fetch(endpoint, {
+    headers: {
+      "X-MICROCMS-API-KEY": c.env["MICROCMS-API-KEY"]
+    }
+  });
+  const messages = await res.json();
+  return c.json(messages);
+};
+
+export const createMessage: Handler<Env> = async (c) => {
+  const payload = await c.req.json<{message: string}>();
+  await fetch(endpoint, {
+    method: "POST",
+    body: JSON.stringify(payload),
+    headers: {
+      "Content-Type": "application/json",
+      "X-MICROCMS-API-KEY": c.env["MICROCMS-API-KEY"]
+    }
+  });
+
+  c.status(201);
+  return c.json({ success: true });
+};
